1b62fbae640228ae179be3f0de897d21e1a1232a
Kyle Leggate <38234353+KyleLeggate@users.noreply.github.com>
2022-01-25 12:43:35 +0000

    Add files via upload

---
+++ VastDataDepositBox/VastDataDepositBox/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace VastDataDepositBox
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnEncrypt = new System.Windows.Forms.Button();
+			this.btnGenMatrix = new System.Windows.Forms.Button();
+			this.txtPlainText = new System.Windows.Forms.TextBox();
+			this.lblKeyOut = new System.Windows.Forms.Label();
+			this.lblPlainText = new System.Windows.Forms.Label();
+			this.txtKey = new System.Windows.Forms.TextBox();
+			this.btnDecrypt = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnEncrypt
+			// 
+			this.btnEncrypt.Location = new System.Drawing.Point(12, 104);
+			this.btnEncrypt.Name = "btnEncrypt";
+			this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+			this.btnEncrypt.TabIndex = 0;
+			this.btnEncrypt.Text = "Encrypt";
+			this.btnEncrypt.UseVisualStyleBackColor = true;
+			this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+			// 
+			// btnGenMatrix
+			// 
+			this.btnGenMatrix.Location = new System.Drawing.Point(12, 12);
+			this.btnGenMatrix.Name = "btnGenMatrix";
+			this.btnGenMatrix.Size = new System.Drawing.Size(75, 23);
+			this.btnGenMatrix.TabIndex = 1;
+			this.btnGenMatrix.Text = "Generate Matrix";
+			this.btnGenMatrix.UseVisualStyleBackColor = true;
+			this.btnGenMatrix.Click += new System.EventHandler(this.btnGenMatrix_Click);
+			// 
+			// txtPlainText
+			// 
+			this.txtPlainText.Location = new System.Drawing.Point(13, 78);
+			this.txtPlainText.Name = "txtPlainText";
+			this.txtPlainText.Size = new System.Drawing.Size(133, 20);
+			this.txtPlainText.TabIndex = 2;
+			// 
+			// lblKeyOut
+			// 
+			this.lblKeyOut.AutoSize = true;
+			this.lblKeyOut.Location = new System.Drawing.Point(13, 134);
+			this.lblKeyOut.Name = "lblKeyOut";
+			this.lblKeyOut.Size = new System.Drawing.Size(28, 13);
+			this.lblKeyOut.TabIndex = 3;
+			this.lblKeyOut.Text = "Key:";
+			// 
+			// lblPlainText
+			// 
+			this.lblPlainText.AutoSize = true;
+			this.lblPlainText.Location = new System.Drawing.Point(13, 256);
+			this.lblPlainText.Name = "lblPlainText";
+			this.lblPlainText.Size = new System.Drawing.Size(31, 13);
+			this.lblPlainText.TabIndex = 6;
+			this.lblPlainText.Text = "Text:";
+			// 
+			// txtKey
+			// 
+			this.txtKey.Location = new System.Drawing.Point(13, 200);
+			this.txtKey.Name = "txtKey";
+			this.txtKey.Size = new System.Drawing.Size(133, 20);
+			this.txtKey.TabIndex = 5;
+			// 
+			// btnDecrypt
+			// 
+			this.btnDecrypt.Location = new System.Drawing.Point(12, 226);
+			this.btnDecrypt.Name = "btnDecrypt";
+			this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+			this.btnDecrypt.TabIndex = 4;
+			this.btnDecrypt.Text = "Decrypt";
+			this.btnDecrypt.UseVisualStyleBackColor = true;
+			this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lblPlainText);
+			this.Controls.Add(this.txtKey);
+			this.Controls.Add(this.btnDecrypt);
+			this.Controls.Add(this.lblKeyOut);
+			this.Controls.Add(this.txtPlainText);
+			this.Controls.Add(this.btnGenMatrix);
+			this.Controls.Add(this.btnEncrypt);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnEncrypt;
+		private System.Windows.Forms.Button btnGenMatrix;
+		private System.Windows.Forms.TextBox txtPlainText;
+		private System.Windows.Forms.Label lblKeyOut;
+		private System.Windows.Forms.Label lblPlainText;
+		private System.Windows.Forms.TextBox txtKey;
+		private System.Windows.Forms.Button btnDecrypt;
+	}
+}
+
---
+++ VastDataDepositBox/VastDataDepositBox/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VastDataDepositBox
+{
+	public partial class Form1 : Form
+	{
+		Random r = new Random();
+		int matrixLength = 512;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnGenMatrix_Click(object sender, EventArgs e)
+		{
+			//matrix
+			string matrix = "";
+			for(int i = 0; i < matrixLength; i++)
+			{
+				matrix += Math.Round(r.NextDouble());
+			}
+
+			// writing   
+			FileStream fs = new FileStream("matrix.vdd", FileMode.OpenOrCreate, FileAccess.Write);
+			byte[] bytes = Encoding.UTF8.GetBytes(matrix);
+			fs.Write(bytes, 0, bytes.Length);
+			fs.Flush();
+			fs.Close();
+			fs.Dispose();
+		}
+
+		private void btnEncrypt_Click(object sender, EventArgs e)
+		{
+			string plain = txtPlainText.Text;
+
+			//generate key
+			int rem = r.Next((matrixLength/4), 3*(matrixLength/4));
+			int mul = r.Next(5, 30) * matrixLength;
+			int byteLength = Encoding.UTF8.GetBytes(plain).Length;
+			int byteLD = byteLength.ToString().Length; //number of digits in the byte array's length
+			int key = int.Parse(byteLD.ToString() + byteLength.ToString() + (mul + rem).ToString());
+			lblKeyOut.Text = key.ToString();
+
+			//get parity sequence
+			int last = 0;
+			int[] pS = new int[key.ToString().Length];
+			int[] kS = key.ToString().Select(c => int.Parse(c.ToString())).ToArray();
+			for(int i = 0; i < key.ToString().Length; i++)
+			{
+				pS[i] = (kS[i]+last) % 2;
+				last = pS[i];
+			}
+
+			//apply parity
+			string hiddenS = "";
+			int[] plainS = plain.Select(c => int.Parse(c.ToString())).ToArray();
+			int ki = 0;
+			for(int i = 0; i < plainS.Length; i++)
+			{
+				hiddenS += (plainS[i] + kS[ki]) % 2;
+				ki++;
+				if(ki >= kS.Length) { ki = 0; }
+			}
+			
+
+			//alter matrix
+			FileStream fs = new FileStream("matrix.vdd", FileMode.Open, FileAccess.Write);
+			fs.Seek(rem, SeekOrigin.Begin);
+			byte[] bytes = Encoding.UTF8.GetBytes(hiddenS);
+			fs.Write(bytes, 0, bytes.Length);
+			fs.Flush();
+			fs.Close();
+			fs.Dispose();
+		}
+
+		private void btnDecrypt_Click(object sender, EventArgs e)
+		{
+			//read key
+			string key = txtKey.Text;
+			char[] keyCh = key.ToCharArray();
+
+			string lenSt = "";
+			for(int i = 1; i < int.Parse(keyCh[0].ToString())+1; i ++)
+			{
+				lenSt += keyCh[i].ToString();
+			}
+			int len = int.Parse(lenSt);
+
+			string offsetSt = "";
+			for(int i = int.Parse(keyCh[0].ToString())+1; i < key.Length; i++)
+			{
+				offsetSt += keyCh[i];
+			}
+			int offset = int.Parse(offsetSt) % matrixLength;
+
+			//read in matrix part
+			FileStream fs = new FileStream("matrix.vdd", FileMode.Open, FileAccess.Read);
+			fs.Seek(offset, SeekOrigin.Begin);
+			byte[] encodedBytes = new byte[len];
+			fs.Read(encodedBytes, 0, len);
+			string encodedText = Encoding.UTF8.GetString(encodedBytes);
+			fs.Flush();
+			fs.Close();
+			fs.Dispose();
+
+			//decode encoded text
+			//get parity sequence
+			int last = 0;
+			int[] pS = new int[key.ToString().Length];
+			int[] kS = key.ToString().Select(c => int.Parse(c.ToString())).ToArray();
+			for(int i = 0; i < key.ToString().Length; i++)
+			{
+				pS[i] = (kS[i] + last) % 2;
+				last = pS[i];
+			}
+
+			//apply parity
+			string decodedText = "";
+			int[] encodedS = encodedText.Select(c => int.Parse(c.ToString())).ToArray();
+			int ki = 0;
+			for(int i = 0; i < encodedS.Length; i++)
+			{
+				decodedText += (encodedS[i] + kS[ki]) % 2;
+				ki++;
+				if(ki >= kS.Length) { ki = 0; }
+			}
+
+			lblPlainText.Text = decodedText;
+		}
+	}
+}
